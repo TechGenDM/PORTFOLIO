5d221a1cb476d24c3105a6d7b8b59570b2ac2959
Devasish Mishra <devasishmishra74@gmail.com>
2026-05-27 15:53:19 +0530

    chore: clean up gitignore and untrack .next build cache

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components/three/SceneWrapper.tsx -> ./Scene\":{\"id\":\"components/three/SceneWrapper.tsx -> ./Scene\",\"files\":[\"static/chunks/_app-pages-browser_components_three_Scene_tsx.js\"]}}"
